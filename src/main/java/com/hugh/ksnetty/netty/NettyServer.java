0165a485c55845b53c680bf6761facaf468ff812
hugh <handovergod@gmail.com>
2020-03-22 11:16:20 +0800

    netty 任务队列分析，模型解读

--- src/main/java/com/hugh/ksnetty/netty/NettyServer.java
+++ src/main/java/com/hugh/ksnetty/netty/NettyServer.java
@@ -12,9 +12,24 @@ import io.netty.channel.socket.nio.NioServerSocketChannel;
 /**
  * @author hugh
  *
+ * 示意流程
  * 1、创建两个线程组
  * 2、创建启动对象并配置参数， 设置业务handle
  * 3、绑定端口并同步，监听关闭
+ *
+ *
+ * [[模型解读]]
+ *
+ * Netty 抽象出两组线程池，BossGroup 专门负责接收客户端连接，WorkerGroup 专门负责网络读写操作。
+ * 1,NioEventLoop 表示一个不断循环执行处理任务的线程，每个 NioEventLoop 都有一个 selector，用于监听绑定在其上的 socket 网络通道。
+ * 2, NioEventLoop 内部采用串行化设计，从消息的读取->解码->处理->编码->发送，始终由 IO 线程 NioEventLoop 负责
+ *
+ * NioEventLoopGroup 下包含多个 NioEventLoop
+ * 	>每个 NioEventLoop 中包含有一个 Selector，一个 taskQueue
+ * 	>每个 NioEventLoop 的 Selector 上可以注册监听多个 NioChannel
+ * 	>每个 NioChannel 只会绑定在唯一的 NioEventLoop 上
+ * 	>每个 NioChannel 都绑定有一个自己的 ChannelPipeline
+ *
  */
 public class NettyServer {
 
@@ -42,6 +57,9 @@ public class NettyServer {
                         // 给pipeline设置handle
                         @Override
                         protected void initChannel(SocketChannel socketChannel) throws Exception {
+
+                            System.out.println("每个用户的socketChannel 不一样的 = " + socketChannel.hashCode());
+
                             socketChannel.pipeline().addLast(new NettyServerHandle());
                         }
                     }); // 设置workGroup 的EventLoop 对应管道的 处理器(可以是 Netty提供，或自定义的)
--- src/main/java/com/hugh/ksnetty/netty/NettyServerHandle.java
+++ src/main/java/com/hugh/ksnetty/netty/NettyServerHandle.java
@@ -6,6 +6,8 @@ import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.ChannelInboundHandlerAdapter;
 import io.netty.util.CharsetUtil;
 
+import java.util.concurrent.TimeUnit;
+
 /**
  * @author hugh
  *
@@ -19,7 +21,8 @@ public class NettyServerHandle extends ChannelInboundHandlerAdapter {
     /**
      * 读取数据事件
      *
-     * ChannelHandlerContext包含 {pipeline}  {channel} {address}
+     * ChannelHandlerContext  重量级对象，包含 {pipeline}  {channel} {address}
+     * 本质上是双向链表
      *
      * @param ctx
      * @param msg
@@ -36,6 +39,43 @@ public class NettyServerHandle extends ChannelInboundHandlerAdapter {
 
         System.out.println("客户端地址=" + ctx.channel().remoteAddress());
 
+        /**
+         * Netty人物队列使用场景:
+         * 1. 用户自定义普通任务
+         * 2. 用户自定义定时任务
+         * 3. 非当前 Ractor 线程调用Channel各种方法( 把所有用户的SocketChannel用集合管理起来，再调用)
+         *
+         *
+         * 假设这里需要处理一个费时的操作，为使不阻塞，演示第一种场景
+         *
+         * 注意：此处可以启动多个线程执行，然而，eventLoop中的task实则还是在一个线程中
+         */
+
+        ctx.channel().eventLoop().execute(() -> {
+            try {
+                TimeUnit.SECONDS.sleep(10);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            System.out.println("哈喽，客户端111");
+        });
+
+        /**
+         * 定时任务场景
+         */
+        ctx.channel().eventLoop().schedule( () -> {
+            try {
+                TimeUnit.SECONDS.sleep(5);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            System.out.println("哈喽，客户端222 From定时任务");
+        }, 5, TimeUnit.SECONDS);
+
+        /**
+         * 这里可以打个断点，debug观察eventLoop中的taskQueue
+         */
+        System.out.println("等待好久了，Let,s go ...");
     }
 
 
